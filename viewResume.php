7408ed526b1719622b939e4ef392508dc6c23315
Renuka <kumari.renuka@st.niituniversity.in>
2018-08-02 15:31:04 +0530

    Second Commit

--- viewResume.php
+++
@@ -1,9 +0,0 @@
-<?php
-header("Content-Length: " . filesize ('MyResume.pdf') );
-header("Content-type: application/pdf");
-header("Content-disposition: inline;
-filename=".basename('MyResume.pdf'));
-header('Expires: 0');
-header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
-$filepath = readfile('MyResume.pdf');
-?>
